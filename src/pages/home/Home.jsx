b00dc0d41bdd247b60e9b52377ea8b87b9ef272c
alisaromantsova <113140050+alisaromantsova@users.noreply.github.com>
2023-08-03 02:25:35 +0300

    add some icons

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import './home.scss';
-import Header from 'components/Header/Header';
+
 import Sidebar from 'components/Sidebar/Sidebar';
+import Header from 'components/Header/Header';
 
 const Home = () => {
   return (
