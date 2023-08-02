889fb5fb26bf2210f7441224cfa4f354b07bd217
Alex <103459735+torn80beta@users.noreply.github.com>
2023-08-03 01:30:03 +0300

    header del

--- src/components/ScreensPage/ScreensPage.jsx
+++ src/components/ScreensPage/ScreensPage.jsx
@@ -5,7 +5,7 @@ const ScreensPage = () => {
   return (
     <div className="screensPage">
       ScreensPage
-      <PopUp>
+      <PopUp modalName={'Add another column'}>
         <p>Screens page popup</p>
       </PopUp>
     </div>
