b20e22836bc9d70859dc761e441a35747a3cc654
natalia-pavlenko <npavl56@gmail.com>
2023-08-03 17:23:20 +0300

    add

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,44 @@
 import './sidebar.scss';
-
+import { ReactComponent as LogoIcon } from './img/icons/logo-icon.svg';
+import { ReactComponent as Project } from './img/icons/Project.svg';
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebarBackdrop">
+      <div className="sidebar">
+        <div className="logoWrapp">
+          <LogoIcon className="logoIcon" />
+          <h1 className="logoName">Task Pro</h1>
+        </div>
+        <div className="customAdviseWrapper">
+          <h3 className="customAdvise">My boards</h3>
+        </div>
+
+        <div className="createBoard">
+          <div className="createBoardTextWrapper">
+            <h2 className="createBoardText">Create a new board</h2>
+          </div>
+
+          <div className="createBoardPlus">
+            <button>plus</button>
+          </div>
+        </div>
+
+        <div className="backGround">
+          <Project className="projectIcon" />
+          <h2 className="projectName">Project office</h2>
+        </div>
+
+        <div className="blockSidebar">
+          {/* <Assets className ="assetsIcon "/> */}
+
+          <p className='taskSidebar'>
+            If you need help with <span className ='taskProSidebar'>TaskPro </span>, check out our support resources or
+            reach out to our customer support team.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,11 +2,11 @@ import './layout.scss';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
-  return (
-    <div className="layout">
+  return <>
+       <div className="layout">
       <Outlet />
     </div>
-  );
+  </>
 };
 
 export default Layout;
