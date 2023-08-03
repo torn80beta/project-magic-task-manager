0116263e7c6293206db267a947de86e621505b6e
Serhii Saproshyn <112710917+Serhii-Saproshyn@users.noreply.github.com>
2023-08-04 00:55:21 +0300

    login

---
+++ src/components/LoginForm/LoginSchema.js
@@ -0,0 +1,19 @@
+import * as Yup from 'yup';
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string()
+    .matches(
+      /^[a-zA-Zа-яА-Я0-9._%+-]+@[a-zA-Zа-яА-Я0-9.-]+\.[a-zA-Zа-яА-Я]{2,}$/,
+      'Invalid email format'
+    )
+    .required('Email is required'),
+  password: Yup.string()
+    .matches(
+      /^[a-zA-Z0-9!@#$%^&*()_+,-./:;<=>?@[\]^_`{|}~]+$/,
+      'Invalid password format'
+    )
+    .min(8, 'Password must be at least 8 characters')
+    .max(64, 'Password must be at most 64 characters')
+    .required('Password is required'),
+});
+export default LoginSchema;
