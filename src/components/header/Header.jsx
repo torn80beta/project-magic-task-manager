9fd29f70c32dbff198e6b6e7cf0846aaa5535b22
Diana Forostiana <dianaforost@gmail.com>
2023-08-04 12:52:33 +0200

    feat: header burger-menu
    fix: styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,9 @@ import './header.scss';
 import Icon from 'components/icon/Icon';
 import { themeState, changeTheme } from 'redux/theme/themeSlice';
 import { useSelector, useDispatch } from 'react-redux';
+import Sidebar from 'components/Sidebar/Sidebar';
 import { selectUserName, selectUserAvatar } from 'redux/auth/auth-slice';
+import PopUp from 'components/modal/PopUp';
 
 const Header = ({ children }) => {
   const themes = ['light', 'dark', 'violet'];
@@ -20,7 +22,6 @@ const Header = ({ children }) => {
   const handleToggleMenu = () => {
     setMenuActive(prevState => !prevState);
   };
-
   const handleOptionClick = label => {
     dispatch(changeTheme(label));
     setMenuActive(false);
@@ -61,48 +62,63 @@ const Header = ({ children }) => {
   }, [isMenuActive]);
 
   return (
-    <div className={`header theme-${theme}`}>
-      <form>
-        <div
-          className={`select ${isMenuActive}`}
-          onClick={handleToggleMenu}
-          ref={selectSingleRef}
-        >
-          <div className={`selectTitle theme-${theme}`} data-default="Theme">
-            Theme
-            <Icon
-              className={`selectIcon theme-${theme}`}
-              id={'chevron-down'}
-              width={16}
-              height={16}
-            />
+    <header className={`header theme-${theme}`}>
+      <PopUp
+        data={
+          <Icon
+            className={`select-icon theme-${theme}`}
+            id={'show-sidebar'}
+            width={24}
+            height={24}
+          />
+        }
+      >
+        <Sidebar />
+      </PopUp>
+
+      <div className="header-wrapper">
+        <form>
+          <div
+            className={`select ${isMenuActive}`}
+            onClick={handleToggleMenu}
+            ref={selectSingleRef}
+          >
+            <div className={`select-title theme-${theme}`} data-default="Theme">
+              Theme
+              <Icon
+                className={`select-icon theme-${theme}`}
+                id={'chevron-down'}
+                width={16}
+                height={16}
+              />
+            </div>
+            <div className={`select-content theme-${theme}`}>
+              {themes.map((themeOption, index) => (
+                <div key={index} className="select-container">
+                  <input
+                    id={`single-select${index}`}
+                    className={`select-input theme-${theme}`}
+                    type="radio"
+                    name="single-select"
+                    value={themeOption}
+                    checked={theme === themeOption}
+                    onChange={() => handleOptionClick(themeOption)}
+                  />
+                  <label
+                    htmlFor={`single-select${index}`}
+                    className={`select-label theme-${theme}`}
+                  >
+                    {themeOption.charAt(0).toUpperCase() + themeOption.slice(1)}
+                  </label>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className={`selectContent theme-${theme}`}>
-            {themes.map((themeOption, index) => (
-              <div key={index} className="selectContainer">
-                <input
-                  id={`singleSelect${index}`}
-                  className={`selectInput theme-${theme}`}
-                  type="radio"
-                  name="singleSelect"
-                  value={themeOption}
-                  checked={theme === themeOption}
-                  onChange={() => handleOptionClick(themeOption)}
-                />
-                <label
-                  htmlFor={`singleSelect${index}`}
-                  className={`selectLabel theme-${theme}`}
-                >
-                  {themeOption.charAt(0).toUpperCase() + themeOption.slice(1)}
-                </label>
-              </div>
-            ))}
-          </div>
-        </div>
-      </form>
-      {children}
-      <UserInfo theme={theme} user={user} />
-    </div>
+        </form>
+        {children}
+        <UserInfo theme={theme} user={user} />
+      </div>
+    </header>
   );
 };
 export default Header;
