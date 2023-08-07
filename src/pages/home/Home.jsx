bf44e9f32bc4fac6270d6122323c06d8b17a53a7
Alex <103459735+torn80beta@users.noreply.github.com>
2023-08-07 06:29:04 +0300

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,8 @@ import Header from 'components/header/Header';
 import { selectCurrentTheme } from 'redux/auth/auth-slice';
 
 import { useSelector } from 'react-redux';
+import PopUp from 'components/modal/PopUp';
+import BoardForm from 'components/boardForm/BoardForm';
 
 const Home = () => {
   // const theme = useSelector(themeState);
@@ -16,6 +18,23 @@ const Home = () => {
       <div className={`home-wrapper theme-${theme}`}>
         <Header />
         <Outlet />
+        <div className={`home-content-wrapper`}>
+          <p className={`home-text theme-${theme}`}>
+            Before starting your project, it is essential{' '}
+            <PopUp
+              data={
+                <span className={`home-text-link theme-${theme}`}>
+                  to create a board
+                </span>
+              }
+            >
+              <BoardForm />
+            </PopUp>{' '}
+            to visualize and track all the necessary tasks and milestones. This
+            board serves as a powerful tool to organize the workflow and ensure
+            effective collaboration among team members.
+          </p>
+        </div>
       </div>
     </div>
   );
