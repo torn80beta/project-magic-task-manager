66f42570c12ef5cf91a7bd9e04d76addcd86a090
Alex <103459735+torn80beta@users.noreply.github.com>
2023-08-07 10:00:37 +0300

    homepage fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useParams } from 'react-router-dom';
 import './home.scss';
 import Sidebar from 'components/Sidebar/Sidebar';
 import Header from 'components/header/Header';
 import { selectCurrentTheme } from 'redux/auth/auth-slice';
 import { useSelector } from 'react-redux';
-// import PopUp from 'components/modal/PopUp';
-// import BoardForm from 'components/boardForm/BoardForm';
+import PopUp from 'components/modal/PopUp';
+import BoardForm from 'components/boardForm/BoardForm';
 
 const Home = () => {
   const theme = useSelector(selectCurrentTheme);
+  const shouldRenderHomePage =
+    Object.keys(useParams()).length > 0 ? false : true;
 
   return (
     <div className="home">
@@ -16,23 +18,25 @@ const Home = () => {
       <div className={`home-wrapper theme-${theme}`}>
         <Header />
         <Outlet />
-        {/* <div className={`home-content-wrapper`}>
-          <p className={`home-text theme-${theme}`}>
-            Before starting your project, it is essential{' '}
-            <PopUp
-              data={
-                <span className={`home-text-link theme-${theme}`}>
-                  to create a board
-                </span>
-              }
-            >
-              <BoardForm />
-            </PopUp>{' '}
-            to visualize and track all the necessary tasks and milestones. This
-            board serves as a powerful tool to organize the workflow and ensure
-            effective collaboration among team members.
-          </p>
-        </div> */}
+        {shouldRenderHomePage && (
+          <div className={`home-content-wrapper`}>
+            <p className={`home-text theme-${theme}`}>
+              Before starting your project, it is essential{' '}
+              <PopUp
+                data={
+                  <span className={`home-text-link theme-${theme}`}>
+                    to create a board
+                  </span>
+                }
+              >
+                <BoardForm />
+              </PopUp>{' '}
+              to visualize and track all the necessary tasks and milestones.
+              This board serves as a powerful tool to organize the workflow and
+              ensure effective collaboration among team members.
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
