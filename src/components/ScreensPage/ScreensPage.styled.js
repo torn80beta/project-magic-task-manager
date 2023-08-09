56f388153e0b558d2d56321ca044a5fd7175568a
alisaromantsova <113140050+alisaromantsova@users.noreply.github.com>
2023-08-10 01:57:56 +0300

    1

--- src/components/ScreensPage/ScreensPage.styled.js
+++ src/components/ScreensPage/ScreensPage.styled.js
@@ -13,12 +13,13 @@ export const BoardContainer = styled.section`
   background-origin: border-box;
   background-repeat: no-repeat;
   background-size: cover;
-  @media screen and (min-width: 1440px) {
-    background-image: url(${props => props.backgroundImg.source.desktop});
-  }
   @media screen and (min-width: 768px) {
     background-image: url(${props => props.backgroundImg.source.tablet});
   }
+  @media screen and (min-width: 1440px) {
+    background-image: url(${props => props.backgroundImg.source.desktop});
+  }
+
   // @media screen and (max-width: 767px) {
   background-image: url(${props => props.backgroundImg.source.mobile});
   // }
