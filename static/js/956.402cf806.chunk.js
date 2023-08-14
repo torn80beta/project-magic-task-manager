432c68544512580d72f951fb8e29f73f7b662ba1
torn80beta <103459735+torn80beta@users.noreply.github.com>
2023-08-14 18:40:06 +0000

    Deploying to gh-pages from @ torn80beta/project-magic-task-manager@d79cd16abc6d2a640e41c1d4cf2f21327c58a0bb 🚀

--- static/js/956.402cf806.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[956],{8956:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184),u=function(){return(0,r.jsx)("div",{className:"notFound",children:"Page Not Found :("})}}}]);
-//# sourceMappingURL=956.402cf806.chunk.js.map
